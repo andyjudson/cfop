3b14902260f5bd50cab7fc5ee441b5501ca9846b
Andy Judson <andy.judson@gmail.com>
2026-03-24 07:27:55 +0000

    Renaming cube image generator app as cubify

--- imggen-app/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Local development tool only - not deployed to GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
